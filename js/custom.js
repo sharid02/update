ca2175270cdabf1264498e00249cba3af0ebcf58
sharid02 <sharidkayes@gmail.com>
2018-02-16 00:41:35 +0600

    first commit

--- js/custom.js
+++ js/custom.js
@@ -11,3 +11,24 @@ $(document).ready(function(){
 $('.navbar-nav>li>a').on('click', function(){
     $('.navbar-collapse').collapse('hide');
 })
+
+// owl js
+$(document).ready(function() {
+ 
+  $("#owl-demo").owlCarousel({
+ 
+      slideSpeed : 300,
+      paginationSpeed : 400,
+      singleItem:true,
+      autoPlay : 5000,
+ 
+      // "singleItem:true" is a shortcut for:
+      // items : 1, 
+      // itemsDesktop : false,
+      // itemsDesktopSmall : false,
+      // itemsTablet: false,
+      // itemsMobile : false
+ 
+  });
+ 
+});
